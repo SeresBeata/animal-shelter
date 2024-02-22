e9eb72cd0288a89b4dd5c1f094b770cbc64777cb
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 20:34:26 +0100

    Update sections of App.js with id
    Update sections of App.js with id, and with it enable the scrolling option by using the sidebar.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <section>
+      <section id="Homepage">
         <Navbar />
+        <img
+          src="https://jankrepl.github.io/assets/images/symbolic_regression/main_files/cute-dog-transparent-background.png"
+          alt="cute dog"
+        />
       </section>
-      <section>Parallax</section>
+      <section id="About">Parallax</section>
       <section>Summary</section>
-      <section>Parallax</section>
+      <section id="Stories">Parallax</section>
       <section>Example1</section>
       <section>Example2</section>
       <section>Example3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
