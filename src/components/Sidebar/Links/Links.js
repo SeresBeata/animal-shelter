172282ec3eff2632eabd1e381af7ac3e44e3a714
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 21:18:27 +0100

    Create Sidebar Menu Link Animations
    Import framer-motion. Create an animation for the opening and closing scenario of the Sidebar. Add the animation by using variants. Use whileHover and whileTap for hovering and clicking effects.

--- src/components/Sidebar/Links/Links.js
+++ src/components/Sidebar/Links/Links.js
@@ -1,16 +1,49 @@
+import { motion } from "framer-motion";
+
+//create consts for item animation
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 //create and export Links child component
 const Links = () => {
   const items = ["Homepage", "About", "Stories", "Contact"];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {/* use array.map() to to render a list of items */}
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
