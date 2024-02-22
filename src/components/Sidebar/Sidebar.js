d19b06f8b99f130b469a56ef81932529846b4b4e
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 16:39:55 +0100

    Create ToggleBtn component
    Create and export ToggleBtn child component. Import it into Sidebar.js file and add to Sidebar.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,11 @@
+//import child components
+import ToggleBtn from "./ToggleBtn/ToggleBtn";
 //create and export Sidebar child component
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-bg"></div>
+      <ToggleBtn />
     </div>
   );
 };
---
+++ src/components/Sidebar/ToggleBtn/ToggleBtn.js
@@ -0,0 +1,6 @@
+//create and export ToggleBtn child component
+const ToggleBtn = () => {
+  return <div> ToggleBtn</div>;
+};
+
+export default ToggleBtn;
