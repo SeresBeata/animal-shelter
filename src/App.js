3abf159cf95446bd8824740922ebd64e21ef31e8
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 10:18:41 +0100

    Create sections at App.js
    Create the main sections of the project at App.js file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Summary</section>
+      <section>Parallax</section>
+      <section>Example1</section>
+      <section>Example2</section>
+      <section>Example3</section>
+      <section>Contact</section>
+    </div>
+  );
 };
 
 export default App;
