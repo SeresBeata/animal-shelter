86a7914c4934e9486105cfdc1624d8af2f37c513
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 20:13:36 +0100

    Update ToggleBtn with function prop
    Extract setOpen prop and call it in case of btn click.

--- src/components/Sidebar/ToggleBtn/ToggleBtn.js
+++ src/components/Sidebar/ToggleBtn/ToggleBtn.js
@@ -1,6 +1,7 @@
 //create and export ToggleBtn child component
-const ToggleBtn = () => {
-  return <div> ToggleBtn</div>;
+const ToggleBtn = ({ setOpen }) => {
+  //use the extracted setOpen prop and call it in case of btn click event
+  return <button onClick={() => setOpen((prev) => !prev)}> ToggleBtn</button>;
 };
 
 export default ToggleBtn;
