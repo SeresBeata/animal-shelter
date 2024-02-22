ee5bc28efa77372c86354e12973eec2f1e722e03
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-22 20:11:08 +0100

    Update Sidebar.js for click-animation
    Import "useState" and "motion".  Create const variants for Sidebar btn click animation. Create state variable called "open" with initial state "false". Use ternary operator, in case of truthy and falsy  "open" state variable. Use the created "variants" const at <div className="sidebar-bg">. Pass setOpen function prop to ToggleBtn component.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,49 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 //import child components
 import ToggleBtn from "./ToggleBtn/ToggleBtn";
 import Links from "./Links/Links";
 
 import "./Sidebar.css";
+
+//create const for Sidebar btn click animation
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 //create and export Sidebar child component
 const Sidebar = () => {
+  //create state variable with initial state "false"
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-bg">
+    //use ternary operator:
+    //- if "open" state variable is truthy, then the Sidebar is "open"
+    //- if "open" state variable is falsy, then the Sidebar is "closed"
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      {/* Use the created "variants" const */}
+      <motion.div className="sidebar-bg" variants={variants}>
         <Links />
-      </div>
-      <ToggleBtn />
-    </div>
+      </motion.div>
+      {/* pass function prop setOpen */}
+      <ToggleBtn setOpen={setOpen} />
+    </motion.div>
   );
 };
 
