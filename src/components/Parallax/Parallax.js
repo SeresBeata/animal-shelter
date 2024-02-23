0e35fb8feeff071ddef90174c87ae7c9064709d3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-23 17:05:37 +0100

    Create Parallax Scrolling Effect
    Import "useRef" from "react" and "motion, useScroll, useTransform" from "framer-motion".  Use useRef() on <div className="parallax">. Use useScroll Hook for Y axis progress. Use useTransform for transitions. Add effect to elements of Parallax component.
    
    Update Parallax.scss with "overflow:hidden"-and with it hide unnecessary parts of the images during scroll effect. Update Parallax.css and Parallax.css.map by using the Live Sass Compiler extension.

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -1,11 +1,29 @@
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import "./Parallax.scss";
 
 //create and export Parallax child component
 const Parallax = ({ type }) => {
+  //use useRef Hook
+  const ref = useRef();
+
+  //use useScroll Hook for Y axis progress
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  //use useTransform Hook to create different transitions
+  //map values - so using these values we can add animation
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     //Use ternary operator for different bg-color according to the "type" of the component
     <div
       className="parallax"
+      ref={ref}
       style={{
         background:
           type === "about"
@@ -14,10 +32,12 @@ const Parallax = ({ type }) => {
       }}
     >
       {/* Use ternary operator for different text according to the "type" of the component */}
-      <h1>{type === "about" ? "Help us!" : "What to do?"}</h1>
-      <div className="mountains"></div>
-      <div className="planets"></div>
-      <div className="stars"></div>
+      <motion.h1 style={{ y: yText }}>
+        {type === "about" ? "Help us!" : "What to do?"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div style={{ y: yBg }} className="planets"></motion.div>
+      <motion.div style={{ x: yBg }} className="stars"></motion.div>
     </div>
   );
 };
