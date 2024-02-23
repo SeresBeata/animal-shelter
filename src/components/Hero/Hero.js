4fde73155e539d2b1479c12d3446a4e24732fd39
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-23 14:11:10 +0100

    Create animation for text of Hero component
    Import "motion" from "framer-motion". Create const textVariants for animation. Add textVariants to elements of Hero component. Add "motion" keyword for the same elements.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,49 @@
+import { motion } from "framer-motion";
 import "./Hero.css";
+
+//create const for text animation
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      straggerChildren: 0.1,
+    },
+  },
+};
 
 //create and export Hero child component
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-wrapper">
-        <div className="hero-container--text">
-          <h2>ANIMAL LOVE</h2>
-          <h1>Let's protect animals!</h1>
-          <div className="hero-btns">
-            <button>See information about animals</button>
-            <button>Contact Shelter</button>
-          </div>
-          <img
+        <motion.div
+          className="hero-container--text"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>ANIMAL LOVE</motion.h2>
+          <motion.h1 variants={textVariants}>Let's protect animals!</motion.h1>
+          <motion.div variants={textVariants} className="hero-btns">
+            <motion.button variants={textVariants}>
+              See information about animals
+            </motion.button>
+            <motion.button variants={textVariants}>
+              Contact Shelter
+            </motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
             src="https://github.com/safak/animated-portfolio/blob/completed/public/scroll.png?raw=true"
             alt="image about scrolling"
           />
-        </div>
+        </motion.div>
       </div>
       <div className="hero-slidingtext">Animal Love Protect Care</div>
       <div className="hero-container--img">
