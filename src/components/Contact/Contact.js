3ca65f156f3cf7110c15d7178e23b156cb9a2185
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-02-26 08:28:57 +0100

    Create animation for Contact component
    Import motion. Create const variants for animation. Add motion keyword for elements of Contact component. Add variants for the same elements. Use whileInView so the animation restarts every time the user scrolls on Contact component.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,24 +1,40 @@
+import { motion } from "framer-motion";
 import "./Contact.scss";
+
+//create const for animation
+const variants = {
+  initial: { y: 500, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.5, staggerChildren: 0.1 },
+  },
+};
 
 //create and export Contact child component
 const Contact = () => {
   return (
-    <div className="contact">
-      <div className="contact-container--text">
-        <h1>Contact a shelter</h1>
-        <div className="item">
+    <motion.div
+      className="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div className="contact-container--text" variants={variants}>
+        <motion.h1 variants={variants}>Contact a shelter</motion.h1>
+        <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
           <span>info@animalshelter@mail.com</span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Address</h2>
           <span> Animal Shelter Ingolstadt </span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
           <span>+49 123 4567</span>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
       <div className="contact-container--form">
         <form>
           <input type="text" placeholder="Name" required />
@@ -27,7 +43,7 @@ const Contact = () => {
           <button>Submit</button>
         </form>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
